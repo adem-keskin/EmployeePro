e73f82dc22ed55aef0397fbd70c8777718c92416
adem-keskin <ademkeskin.de@gmail.com>
2023-01-28 13:07:04 +0100

    Version 1.0

--- src/main/java/EmployeeRunner.java
+++ src/main/java/EmployeeRunner.java
@@ -2,7 +2,7 @@ public class EmployeeRunner {               // Calisan SIIFI ´ na ait runner cl
 
         public static void main(String[] args) {
 
-            Employee employee1 = new Employee("Kemal",2000,45,1985);    // Calisan SINIFINDAN -> yeni bir Calisan OBJECT
+            Employee employee1 = new Employee("Hasan",2000,45,1985);    // Calisan SINIFINDAN -> yeni bir Calisan OBJECT
             employee1.toString(employee1);      // toString (); method ile Calisan1 = Employee1 ait data yazdirdim
         }
 }
